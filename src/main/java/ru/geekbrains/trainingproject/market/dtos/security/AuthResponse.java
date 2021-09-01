1c738a5e59fbd66e48accb01f0a2875f9ad40ae8
OldBrain <hell_brain@mail.ru>
2021-09-01 14:02:32 +0400

    ДЗ11

---
+++ src/main/java/ru/geekbrains/trainingproject/market/dtos/security/AuthResponse.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.trainingproject.market.dtos.security;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+@NoArgsConstructor
+@Data
+
+public class AuthResponse {
+    private String token;
+}
