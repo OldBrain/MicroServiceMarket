cb6d0906fa5357eaad6ab4e6ebe924f8f9e50fb8
OldBrain <hel_brain@mail.ru>
2021-09-19 19:57:17 +0400

    Корзина работает на удаленном сервисе redis или локальго га Memurai .

--- src/main/java/ru/geekbrains/trainingproject/market/config/security/RedisConfig.java
+++ src/main/java/ru/geekbrains/trainingproject/market/config/security/RedisConfig.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.trainingproject.market.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisPassword;
@@ -13,13 +14,23 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
+    @Value("${redis.port}")
+    int port;
+    @Value("${redis.hostname}")
+    String hostname;
+    @Value("${redis.password}")
+    String password;
 
     @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
-//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
-//        redisStandaloneConfiguration.setPassword(RedisPassword.of("yourRedisPasswordIfAny"));
-//        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    public JedisConnectionFactory jedisConnectionForRedisServiceFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(hostname, port);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionForMemurai() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
         return new JedisConnectionFactory();
     }
 
@@ -28,7 +39,16 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(jedisConnectionForRedisServiceFactory());
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> memuraiTemplete(){
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setConnectionFactory(jedisConnectionForMemurai());
         return template;
     }
 }
