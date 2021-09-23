3a2b1325458cc6f547ba7c368611345c092b8b1a
OldBrain <hell_brain@mail.ru>
2021-09-23 23:43:16 +0400

    ДЗ Готово.

--- src/main/java/ru/geekbrains/trainingproject/market/utils/StatisticUtil.java
+++ src/main/java/ru/geekbrains/trainingproject/market/utils/StatisticUtil.java
@@ -3,17 +3,26 @@ package ru.geekbrains.trainingproject.market.utils;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 @Data
+
+// TODO Сейчас это довольно бесполезная статистика.
+//  Надо добавить количество обращений и среднее время работы
+//  сервисов и методов, если будет полезно можно хранить в  radis
+//  если нет удалить так как на мой взгляд просто замедляет работу.
+//  Вопрос нужна ли здесь ConcurrentHashMap? или можно обойтись обычной?
+
+
 public class StatisticUtil {
     private ConcurrentHashMap<String, ConcurrentHashMap<String, Long>> serviceMap = new ConcurrentHashMap<>();
 
     public void setServiceMap(String serviceName, String methodName, Long duration) {
         //если уже были обе записи
         if (serviceMap.containsKey(serviceName) && serviceMap.get(serviceName).containsKey(methodName)) {
-            Long tmpDuration = serviceMap.get(serviceName).get(methodName) + duration;
+            Long tmpDuration = serviceMap.get(serviceName).get(methodName);
             serviceMap.get(serviceName).put(methodName, tmpDuration);
             return;
         }
@@ -24,7 +33,6 @@ public class StatisticUtil {
             serviceMap.get(serviceName).put(methodName, duration);
             return;
         }
-
         //если была запись о сервисе но новый метод
         if (serviceMap.containsKey(serviceName) && !serviceMap.get(serviceName).containsKey(methodName)) {
             serviceMap.get(serviceName).put(methodName, duration);
